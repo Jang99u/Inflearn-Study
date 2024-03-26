03d93579339bb4fe4d54c304597cf9c2044eed1f
Jang99u <alswkdrb@dgu.ac.kr>
2024-03-26 18:50:42 +0900

    fix: user entity 등록

--- library-app/src/main/java/com/group/libraryapp/domain/user/User.java
+++ library-app/src/main/java/com/group/libraryapp/domain/user/User.java
@@ -1,9 +1,17 @@
 package com.group.libraryapp.domain.user;
 
+import javax.persistence.*;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id = null;
+    @Column(nullable = false, length = 20, name = "name")
     private String name;
     private Integer age;
 
+    protected User() {}
     public User(String name, Integer age) {
         if (name == null || name.isBlank()) {
             throw new IllegalArgumentException(String.format("잘못된 이름이 들어왔습니다."));
@@ -18,5 +26,13 @@ public class User {
 
     public Integer getAge() {
         return age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void updateName(String name) {
+        this.name = name;
     }
 }
