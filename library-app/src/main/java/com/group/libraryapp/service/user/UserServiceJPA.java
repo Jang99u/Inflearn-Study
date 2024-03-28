0257332746cc03d7ca79af3f44097e04ee553bae
Jang99u <alswkdrb@dgu.ac.kr>
2024-03-28 16:04:46 +0900

    fix: UserService 트랜잭션 추가

--- library-app/src/main/java/com/group/libraryapp/service/user/UserServiceJPA.java
+++ library-app/src/main/java/com/group/libraryapp/service/user/UserServiceJPA.java
@@ -6,6 +6,7 @@ import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -18,24 +19,27 @@ public class UserServiceJPA {
         this.userRepository = userRepository;
     }
 
+    @Transactional
     public void saveUser(UserCreateRequest userCreateRequest) {
         userRepository.save(new User(userCreateRequest.getName(), userCreateRequest.getAge()));
     }
 
+    @Transactional(readOnly = true)
     public List<UserResponse> getUsers() {
         return userRepository.findAll().stream()
                 .map(UserResponse::new)
                 .collect(Collectors.toList());
     }
 
+    @Transactional
     public void updateUser(UserUpdateRequest userUpdateRequest) {
         User user = userRepository.findById(userUpdateRequest.getId())
                 .orElseThrow(IllegalAccessError::new);
 
         user.updateName(userUpdateRequest.getName());
-        userRepository.save(user);
     }
 
+    @Transactional
     public void deleteUser(String name) {
         User user = userRepository.findByName(name)
                 .orElseThrow(IllegalArgumentException::new);
